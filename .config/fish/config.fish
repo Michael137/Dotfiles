e6d848a94f605fd6aaeb4a8bc15872f5c0abf52a
Michael Buch <michaelbuch12@gmail.com>
2018-04-22 20:42:27 +0100

    config.fish add path and display vars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,3 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
-
 # Customizations by 137
 set -g theme_display_git yes
 set -g theme_display_git_dirty yes
@@ -32,10 +24,14 @@ set -g theme_title_use_abbreviated_path no
 set -g theme_date_format "+%a %H:%M"
 set -g theme_avoid_ambiguous_glyphs yes
 set -g theme_powerline_fonts yes
-set -g theme_nerd_fonts yes
+set -g theme_nerd_fonts no
 set -g theme_show_exit_status yes
 set -g default_user your_normal_user
 set -g theme_color_scheme dark
 set -g fish_prompt_pwd_dir_length 0
 set -g theme_project_dir_length 1
 set -g theme_newline_cursor yes
+
+set -xg DISPLAY :0
+set -xg PATH /home/gardei/Compilers/gcc/install/bin $PATH
+set -xg PATH /home/gardei/Compilers/clang/install/bin $PATH
